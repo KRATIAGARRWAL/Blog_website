9599ec9303e9b6ad6935482f8728475f20395f9f
Krati Agarwal <kratiag190@gmail.com>
2024-12-09 18:50:34 +0530

    mongo db

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 import express from "express"
 import mongoose from "mongoose";
 const server=express()
+import 'dotenv/config'
 let PORT=3000;
 
-mongoose.connect()
+mongoose.connect(process.env.DB_LOCATION,{ autoIndex:true})
+
+server.post("/signup", (req,res)=>{
+    console.log(req.body)
+    res.json(req.body)
+})
 
 server.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
