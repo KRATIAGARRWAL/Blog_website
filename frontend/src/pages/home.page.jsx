49c72343bc5df6615b981b2abd8f2caefeed9db3
Krati Agarwal <kratiag190@gmail.com>
2025-02-16 18:27:36 +0530

    filtering of blogs

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -4,24 +4,71 @@ import InPageNavigation from "../components/inpage-navigation.component";
 import axios from "axios"
 import Loader from "../components/loader.component"
 import BlogPostCard from "../components/blog-post.component";
+import MinimalBlogPost from "../components/nobanner-blog-post.component";
+import { activeTabRef } from "../components/inpage-navigation.component";
 
-const HomePage= ()=>{
+const HomePage = () => {
 
-    let [blogs, setBlog]=useState(null)
+    let [blogs, setBlog] = useState(null)
+    let [trendingBlogs, setTrendingBlogs] = useState(null);
+    let [pageState, setPageState]=useState("home")
 
-    const fetchLatestBlogs = ()=>{
-        axios.get(import.meta.env.VITE_SERVER_DOMAIN+"/latest-blogs")
-        .then(({data})=>{
-            setBlog(data.blogs)
-        })
-        .catch(err=>{
-            console.log(err)
-        })
+    let categories = ["programming", "hollywood", "film making", "social media", "cooking", "technology", "finance", "tech", "travel"]
+
+    const fetchLatestBlogs = () => {
+        axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
+            .then(({ data }) => {
+                setBlog(data.blogs)
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }
 
-    useEffect(()=>{
+    const fetchBlogsByCategory=()=>{
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/search-blogs", {tag: pageState})
+            .then(({ data }) => {
+                setBlog(data.blogs)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    const fetchTrendingBlogs = () => {
+        axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/trending-blogs")
+            .then(({ data }) => {
+                setTrendingBlogs(data.blogs)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    const loadBlogByCategory=(e)=>{
+        let category =e.target.innerText.toLowerCase();
+        setBlog(null);
+
+        if(pageState==category){
+            setPageState("home");
+            return ;
+        }
+
+        setPageState(category)
+
+    }
+
+    useEffect(() => {
+
+        activeTabRef.current.click()
+        if(pageState=="home")
         fetchLatestBlogs()
-    },[])
+        else {
+            fetchBlogsByCategory()
+        }
+        if(!trendingBlogs)
+        fetchTrendingBlogs();
+    }, [pageState])
 
     return (
         <AnimationWrapper>
@@ -29,26 +76,62 @@ const HomePage= ()=>{
                 {/* latest blogs */}
                 <div className="w-full">
 
-                    <InPageNavigation routes={["home", "trending blogs"]} defaultHidden={["trending blogs"]} >
+                    <InPageNavigation routes={[pageState, "trending blogs"]} defaultHidden={["trending blogs"]} >
                         <>
                             {
-                                blogs ==null ? <Loader/>:
-                                blogs.map((blog,i)=>{
-                                    return <AnimationWrapper transition={{duration : 1, delay : i*.1}} key={i}>
-                                        <BlogPostCard content={blog} author={blog.author.personal_info}  />
-                                    </AnimationWrapper>
-                                })
+                                blogs == null ? <Loader /> :
+                                    blogs.map((blog, i) => {
+                                        return <AnimationWrapper transition={{ duration: 1, delay: i * .1 }} key={i}>
+                                            <BlogPostCard content={blog} author={blog.author.personal_info} />
+                                        </AnimationWrapper>
+                                    })
                             }
                         </>
 
-                        <h1>Trending Blogs Here</h1>
+                        {
+                            trendingBlogs == null ? <Loader /> :
+                                trendingBlogs.map((blog, i) => {
+                                    return <AnimationWrapper transition={{ duration: 1, delay: i * .1 }} key={i}>
+                                        <MinimalBlogPost blog={blog} index={i} />
+                                    </AnimationWrapper>
+                                })
+                        }
                     </InPageNavigation>
 
                 </div>
 
                 {/* filters and trending blogs */}
-                <div>
+                <div className="min-w-[40%] lg:min-w-[400px] max-w-min border-1 border-grey pl-8 pt-3 max-md:hidden ">
 
+                    <div className="flex flex-col gap-10 ">
+                        <div>
+                            <h1 className="font-medium text-xl mb-8">
+                                Stories from all interests</h1>
+
+                            <div className="flex gap-3 flex-wrap">
+                                {
+                                    categories.map((category, i) => {
+                                        return <button onClick={loadBlogByCategory} className={"tag "  +(pageState==category ? "bg-black text-white":" ")}>{category}</button>
+                                    })
+                                }
+                            </div>
+                        </div>
+
+
+                        <div>
+                            <h1 className="font-medium text-xl mb-8">Trending <i className="fi fi-rr-arrow-trend-up"></i></h1>
+
+                            {
+                                trendingBlogs == null ? <Loader /> :
+                                    trendingBlogs.map((blog, i) => {
+                                        return <AnimationWrapper transition={{ duration: 1, delay: i * .1 }} key={i}>
+                                            <MinimalBlogPost blog={blog} index={i} />
+                                        </AnimationWrapper>
+                                    })
+                            }
+                        </div>
+
+                    </div>
                 </div>
 
             </section>
